3efb2983189c41569780128995c941839a5ff6b8
Joseph <thejosephsamuel@gmail.com>
2022-12-11 22:36:34 +0100

    Complet the style

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # library
+This is one of my first project in the javascript section. It took me a while to gather courage to embark on the project. I am happy i completed it. Thanks to odin
--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const form = document.querySelector(".my-form");
 const add_book = document.querySelector('.btn');
 const con = document.querySelector(".card-container");
 form.style.display = 'none';
-
 add_book.addEventListener('click', () => {
     const display = form.style.display == 'block'? 'none':'block';
     form.style.display = display;
@@ -25,12 +24,11 @@ function createBook(){
         const para_read = document.createElement("p");
         const para_remove = document.createElement("p");
         const remove_btn = document.createElement('button');
-        remove_btn.classList.add("card");
+        remove_btn.classList.add("card-button");
+        div.classList.add("cards");
         remove_btn.textContent = "Remove";
         para_remove.appendChild(remove_btn);
-        div.append(para_title, para_author, para_page, para_read, para_remove);
-       
-        
+        div.append(para_title, para_author, para_page, para_read, para_remove);        
         remove_btn.addEventListener("submit", (event)=> {
             event.preventDefault();
         });
@@ -43,14 +41,18 @@ function createBook(){
         para_page.textContent = book.page  + " pages";
         if(book.read === "on"){
             para_read.textContent = "Read";
+            para_read.style.color = "lightgreen";
         } else{
             para_read.textContent = "Not Read";
+            para_read.style.color = "red";
         }
         para_read.addEventListener("click", ()=>{
             if(para_read.textContent == "Read"){
-                para_read.textContent = "Not Read"
+                para_read.textContent = "Not Read";
+                para_read.style.color = "red";
             }else{
-                para_read.textContent = "Read"
+                para_read.textContent = "Read";
+                para_read.style.color = "lightgreen";
             }
         })
         con.appendChild(div);
